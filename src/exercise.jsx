9f743b091d669af656a10ad6df97068e9a838881
Barick Muhammad Dziellan <barickmdziellan@gmail.com>
2026-04-28 04:42:25 +0700

    async

--- src/exercise.jsx
+++ src/exercise.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserCard from "./UserCard";
+import { users , getUsers} from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
+  	
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
@@ -9,9 +24,9 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard name="Barick" email="barick@gmail.com" street="Jl. Gemah" city="Tegal"/>
-          <UserCard name="Muhammad" email="muhammad@gmail.com" street="Jl. Gemah" city="Semarang"/>
-          <UserCard name="Dziellan" email="dziellan@gmail.com" street="Jl. Gemah" city="Pati"/>
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </div>
     </>
