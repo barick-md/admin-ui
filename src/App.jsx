a51828eccf4cc9242dc8e5133839c6e2280e3bc6
barick-md <barickmdziellan@gmail.com>
2026-07-11 04:53:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import SignIn from "./pages/signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Error from "./pages/Error";
 import Dashboard from "./pages/Dashboard";
